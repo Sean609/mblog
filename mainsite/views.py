0694e40819d245826dd880937b2f1ff67bbcc7db
Sean609 <abliz@bluemouse.com.cn>
2018-09-14 05:08:02 -0700

    first commit

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.template.loader import get_template
+#from __future__ import unicode_literals
 from django.http import HttpResponse
 from .models import Post
+from datetime import datetime
 
 from django.shortcuts import render
 import sys
@@ -10,10 +12,8 @@ sys.setdefaultencoding('utf8')
 
 # Create your views here.
 def homepage(request):
-    
+    template = get_template('index.html')
     posts = Post.objects.all()
-    post_lists = list()
-    for count,post in enumerate(posts):
-        post_lists.append("No.{}:".format(str(count)) + str(post)+"<br>")
-        post_lists.append("<small>" + str(post.body.encode('utf-8')) +"</small><br><br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
